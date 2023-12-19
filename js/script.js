28045fca5e7a06340063d2b55a9f6d3f873184b1
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-19 16:26:21 +0100

    get the elements

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Prendiamo gli elementi dal DOM
+const inputNameSurname = document.getElementById('input-name-surname');
+const inputKilometres = document.getElementById('user-kilometres');
+const inputAge = document.getElementById('user-age');
+const buttonGenerate = document.getElementById('button-generate');
+const buttonCancel = document.getElementById('button-cancel');
+
