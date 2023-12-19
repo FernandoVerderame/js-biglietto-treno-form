a5fa5c49e3382c777ee3bbc15ae280a534e23807
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-19 18:28:33 +0100

    added form input, button & refactoring javascript

--- js/script.js
+++ js/script.js
@@ -4,32 +4,51 @@ const inputKilometres = document.getElementById('user-kilometres');
 const inputAge = document.getElementById('user-age');
 const buttonGenerate = document.getElementById('button-generate');
 const buttonCancel = document.getElementById('button-cancel');
+const resultPrice = document.getElementById('result-price');
+const resultNameSurname = document.getElementById('result-name-surname');
+const numberTrain = document.getElementById('number-train');
+const numberTicket = document.getElementById('number-ticket');
 
-// Controllo scelta
-let errorMessage;
+// Mi metto in ascolto del click sul bottone
+buttonGenerate.addEventListener('click', function(){
+    // Recupero il valore dall'input 
+    const userNameSurname = inputNameSurname.value;
+    const userNumberKilometres = parseInt(inputKilometres.value);
+    const userNumberAge = parseInt(inputAge.value); 
+    const randomNumberTrain = Math.floor(Math.random() * 15) + 1;
+    const randomNumberTicket = Math.floor(Math.random() * 99999) +1;
 
-if (isNaN(userKilometres) || isNaN(userAge) || inputKilometres <= 0 || inputAge <= 0)
-errorMessage = 'Devi inserire un numero valido in entrambi i campi!';
+    // Controllo scelta
+    let errorMessage;
 
-// Calcolare il prezzo totale del viaggio
-if (errorMessage) {
-    alert(errorMessage);
-} else {
+    if (isNaN(userNumberKilometres) || isNaN(userNumberAge) || userNumberKilometres <= 0 || userNumberAge <= 0)
+    errorMessage = 'Devi inserire un numero valido in entrambi i campi!';
 
-    let totalPrice = inputKilometres * 0.21;
+    // Calcolare il prezzo totale del viaggio
+    if (errorMessage) {
+        alert(errorMessage);
+    } else {
 
-    // Sconto del 20% per gli under 18 e del 40% per gli over 65
-    let discount20 = totalPrice * 20 / 100;
-    let discount40 = totalPrice * 40 / 100;
+        let totalPrice = userNumberKilometres * 0.21;
 
-    let discountPrice;
+        // Sconto del 20% per gli under 18 e del 40% per gli over 65
+        let discount20 = totalPrice * 20 / 100;
+        let discount40 = totalPrice * 40 / 100;
 
-    if (inputAge < 18) {
-        totalPrice = totalPrice - discount20;
-    } else if (inputAge > 65) {
-        totalPrice = totalPrice - discount40;
+        let discountPrice;
+
+        if (userNumberAge < 18) {
+            totalPrice = totalPrice - discount20;
+        } else if (userNumberAge > 65) {
+            totalPrice = totalPrice - discount40;
+        }
+
+        // Prezzo finale (con massimo due decimali)
+        totalPrice = totalPrice.toFixed(2);
+
+        resultPrice.innerText = totalPrice;
+        resultNameSurname.innerText = userNameSurname;
+        numberTrain.innerText = randomNumberTrain;
+        numberTicket.innerText = randomNumberTicket;
     }
-
-    // Prezzo finale (con massimo due decimali)
-    totalPrice = totalPrice.toFixed(2);
-}
+})
