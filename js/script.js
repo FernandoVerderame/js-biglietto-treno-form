c29c5a0fa64fb2438723691de3dd0837e0aa7fba
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-19 16:34:55 +0100

    added user choices validation

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ const inputAge = document.getElementById('user-age');
 const buttonGenerate = document.getElementById('button-generate');
 const buttonCancel = document.getElementById('button-cancel');
 
+// Controllo scelta
+let errorMessage;
+
+if (isNaN(userKilometres) || isNaN(userAge) || inputKilometres <= 0 || inputAge <= 0)
+errorMessage = 'Devi inserire un numero valido in entrambi i campi!';
